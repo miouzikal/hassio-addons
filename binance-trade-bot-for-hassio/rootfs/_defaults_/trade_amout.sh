fbb076a99724b74b6d587397da4c9cf523558437
miouzikal <46767572+miouzikal@users.noreply.github.com>
2021-05-21 15:35:05 -0400

    Relocate trade_amout.sh

--- binance-trade-bot-for-hassio/rootfs/_defaults_/trade_amout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while read -r i; do
-sqlite3 data/crypto_trading.db <<EOF
-SELECT "alt_coin_id", "alt_trade_amount" ,"datetime" FROM (SELECT * FROM 'trade_history' WHERE alt_coin_id="$i") WHERE selling = FALSE ORDER BY datetime DESC
-EOF
-done < ../app/supported_coin_list
